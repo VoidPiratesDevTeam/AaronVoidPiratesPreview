26f06eac0688a58652ad0db4fdddcf581582f33f
Aaron Schif <aaronschif@gmail.com>
2014-10-02 11:49:44 -0500

    Update gulpfile.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,29 +1,44 @@
-var gulp = require('gulp');
-var inject = require('gulp-inject');
-var server = require('gulp-express'),
+var gulp = require('gulp'),
+    inject = require('gulp-inject'),
+    server = require('gulp-express'),
     merge = require('merge-stream'),
-    browserify = require('gulp-browserify');
+    browserify = require('gulp-browserify'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat');
+
+var DEBUG = true;
+
+var clientJS = function() {
+    var clientLibraries = gulp.src('./client/index.js', { read: false })
+        .pipe(browserify({
+            debug: DEBUG,
+            insertGlobals: DEBUG,
+        }));
+
+    var externalLibraries = gulp.src([
+        './node_modules/long/dist/Long.js',
+        './node_modules/bytebuffer/dist/ByteBufferAB.js',
+        './node_modules/protobufjs/dist/ProtoBuf.js',
+        './node_modules/socket.io-client/socket.io.js',
+        './bower_components/Keypress/keypress.js',
+        './bower_components/pixi/bin/pixi.dev.js',
+        './node_modules/underscore/underscore.js'
+    ]);
+
+    var result = merge(clientLibraries, externalLibraries);
+
+    if (!DEBUG) {
+        result = result
+            .pipe(concat('client.js'))
+            .pipe(uglify());
+    }
+
+    return result;
+};
 
 
 gulp.task('index', function () {
     var target = gulp.src('./client/index.html');
-    var browserified = gulp.src('./client/index.js')
-        .pipe(browserify({
-            debug: true,
-            insertGlobals: true,
-        }));
-
-    var jsSources = gulp.src(
-        [
-            './node_modules/long/dist/Long.js',
-            './node_modules/bytebuffer/dist/ByteBufferAB.js',
-            './node_modules/protobufjs/dist/ProtoBuf.js',
-            './node_modules/socket.io-client/socket.io.js',
-            './bower_components/Keypress/keypress.js',
-            './bower_components/pixi/bin/pixi.dev.js',
-            './node_modules/underscore/underscore.js'
-        ]
-    )
 
     var otherSources = gulp.src(
         [
@@ -32,7 +47,7 @@ gulp.task('index', function () {
         ]
     )
 
-    var sources = merge(browserified, jsSources, otherSources)
+    var sources = merge(clientJS(), otherSources)
         .pipe(gulp.dest('./_build/client/static/'));
 
     return target.pipe(inject(sources, {ignorePath: '_build/client/'}))
